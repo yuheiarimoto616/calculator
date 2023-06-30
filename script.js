83b30e8096afe3c464af9f2a69d7403839de7e1c
yuheiarimoto616 <yuhei61627@icloud.com>
2023-07-01 04:09:16 +0900

    added all clear and clear functionality

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let userInput = {
-    x: '0',
-    operator: null,
-    y: null
-};
-
-let result;
-
-
 function add(x, y) {
     return x + y;
 }
@@ -23,6 +14,10 @@ function divide(x, y) {
     return x / y;
 }
 
+function modulo(x, y) {
+    return x % y;
+}
+
 function operate(op, x, y) {
     if (op == '+') {
         return add(x, y);
@@ -30,6 +25,8 @@ function operate(op, x, y) {
         return subtract(x, y);
     } else if (op == '×') {
         return multiply(x, y);
+    } else if (op == '%') {
+        return modulo(x, y);
     } else {
         return divide(x, y);
     }
@@ -37,6 +34,12 @@ function operate(op, x, y) {
 
 function updateDisplay(e) {
     if (e.target.className == 'digits') {
+        if (result != null) {
+            userInput.x = 0;
+            userInput.operator = null;
+            userInput.y = null;
+        }
+
         if (userInput.operator != null) {
             if (userInput.y == null) {
                 userInput.y = e.target.textContent;
@@ -61,14 +64,83 @@ function updateDisplay(e) {
         }
         userInput.operator = e.target.textContent;
     }
-    
+
+    result = null;
+    let resultDisplay = document.querySelector('.results');
+    resultDisplay.innerHTML = '&nbsp;';
 
     let inputDisplay = document.querySelector('.inputs');
     inputDisplay.textContent = Object.values(userInput).join(" ");
 }
 
+function displayResult() {
+    if (userInput.y != null) {
+        result = operate(userInput.operator, Number.parseInt(userInput.x), Number.parseInt(userInput.y));
+        let resultDisplay = document.querySelector('.results');
+        resultDisplay.textContent = result;
+    }
+}
+
+function clearAll() {
+    userInput = {
+        x: '0',
+        operator: null,
+        y: null
+    };
+
+    result = null;
+    let resultDisplay = document.querySelector('.results');
+    resultDisplay.innerHTML = '&nbsp;';
+
+    let inputDisplay = document.querySelector('.inputs');
+    inputDisplay.textContent = Object.values(userInput).join(" ");
+}
+
+function backspace() {
+    result = null;
+    let resultDisplay = document.querySelector('.results');
+    resultDisplay.innerHTML = '&nbsp;';
+
+    if (userInput.y != null) {
+        if (userInput.y.length == 1) {
+            userInput.y = null;
+        } else {
+            userInput.y = userInput.y.slice(0, userInput.y.length - 1);
+        }
+    } else if (userInput.operator != null) {
+        userInput.operator = null;
+    } else {
+        if (userInput.x.length == 1) {
+            userInput.x = '0';
+        } else {
+            userInput.x = userInput.x.slice(0, userInput.x.length - 1);
+        }
+    }
+
+    let inputDisplay = document.querySelector('.inputs');
+    inputDisplay.textContent = Object.values(userInput).join(" ");
+}
+
+let userInput = {
+    x: '0',
+    operator: null,
+    y: null
+};
+
+let result = null;
+
 let btns = document.querySelectorAll('.digits, .operator');
+let equal = document.querySelector('.equal');
+let allClear = document.getElementById('ac');
+let clear = document.getElementById('c');
 
 btns.forEach(btn => {
     btn.addEventListener('click', updateDisplay);
 });
+
+equal.addEventListener('click', displayResult);
+
+allClear.addEventListener('click', clearAll);
+
+clear.addEventListener('click', backspace);
+
