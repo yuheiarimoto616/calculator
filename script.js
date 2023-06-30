532f5fd562b357cdacbbde426fa0f260045e8f48
yuheiarimoto616 <yuhei61627@icloud.com>
2023-06-30 23:29:51 +0900

    finished execute operators function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let userInput = {
+    x: '0',
+    operator: null,
+    y: null
+};
+
+let result;
+
+
 function add(x, y) {
     return x + y;
 }
@@ -19,9 +28,47 @@ function operate(op, x, y) {
         return add(x, y);
     } else if (op == '-') {
         return subtract(x, y);
-    } else if (op == '*') {
+    } else if (op == '×') {
         return multiply(x, y);
     } else {
         return divide(x, y);
     }
 }
+
+function updateDisplay(e) {
+    if (e.target.className == 'digits') {
+        if (userInput.operator != null) {
+            if (userInput.y == null) {
+                userInput.y = e.target.textContent;
+            } else {
+                if (userInput.y == '0' && e.target.id != 'zero') {
+                    userInput.y = e.target.textContent;
+                } else if (userInput.y != '0') {
+                    userInput.y += e.target.textContent;
+                }
+            }
+        } else {
+            if (userInput.x == '0' && e.target.id != 'zero') {
+                userInput.x = e.target.textContent;
+            } else if (userInput.x != '0') {
+                userInput.x += e.target.textContent;
+            }
+        }
+    } else if (e.target.className == 'operator') {
+        if (userInput.y != null) {
+            userInput.x = operate(userInput.operator, Number.parseInt(userInput.x), Number.parseInt(userInput.y));
+            userInput.y = null;
+        }
+        userInput.operator = e.target.textContent;
+    }
+    
+
+    let inputDisplay = document.querySelector('.inputs');
+    inputDisplay.textContent = Object.values(userInput).join(" ");
+}
+
+let btns = document.querySelectorAll('.digits, .operator');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', updateDisplay);
+});
